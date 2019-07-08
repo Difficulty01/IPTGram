491f83ca2102c4b6a84e52dfaae2f69f70854331
Rosario <bruno.silva.rosario@gmail.com>
2019-07-08 23:48:38 +0100

    UI first design

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,8 +5,15 @@ namespace IPTGram.Controllers
     public class HomeController : Controller
     {
         [HttpGet("")]
+        [HttpGet("/Home/Index")]
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpGet("/Home/Details")]
+        public IActionResult Details(int ?id)
+        {
+            ViewBag.id = id;
             return View();
         }
     }
--- Startup.cs
+++ Startup.cs
@@ -137,7 +137,8 @@ namespace IPTGram
                 // Permitir conventions-based routing.
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                    );
             });
         }
     }
